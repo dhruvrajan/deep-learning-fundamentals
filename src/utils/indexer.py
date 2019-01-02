5f865d2a7e72cb388b486775410637ba6b85c6f5
Dhruv Rajan <dhruv@krishnaprem.com>
2019-01-02 14:19:00 -0500

    utilities for loading glove embeddings

---
+++ src/utils/indexer.py
@@ -0,0 +1,58 @@
+class Indexer:
+    UNK_SYMBOL = "<UNK>"
+    PAD_SYMBOL = "<PAD>"
+
+    def __init__(self, obj_type=str, idx_type=int):
+        self.obj2int = {}
+        self.int2obj = {}
+
+        assert obj_type != idx_type
+        self.obj_type = obj_type
+        self.idx_type = idx_type
+
+    def has_idx(self, idx):
+        assert type(idx) == self.idx_type
+        return idx in self.int2obj
+
+    def has_obj(self, obj):
+        assert type(obj) == self.obj_type
+        return obj in self.obj2int
+
+    def get_obj(self, idx):
+        if self.has_idx(idx):
+            return self.int2obj[idx]
+        return None
+
+    def get_idx(self, obj):
+        if self.has_obj(obj):
+            return self.obj2int[obj]
+        elif self.has_obj(Indexer.UNK_SYMBOL):
+            return self.obj2int[Indexer.UNK_SYMBOL]
+        return -1
+
+    def add(self, obj):
+        if self.has_obj(obj):
+            return self.get_idx(obj)
+
+        idx = len(self.obj2int)
+        self.obj2int[obj] = idx
+        self.int2obj[idx] = obj
+        return idx
+
+    @staticmethod
+    def create_indexer(with_symbols=True):
+        indexer = Indexer()
+        if with_symbols:
+            indexer.add(Indexer.PAD_SYMBOL)
+            indexer.add(Indexer.UNK_SYMBOL)
+
+        return indexer
+
+    def __getitem__(self, item):
+        assert type(item) in (self.obj_type, self.idx_type)
+
+        if type(item) == self.obj_type:
+            return self.get_idx(item)
+
+        elif type(item) == self.idx_type:
+            return self.get_obj(item)
